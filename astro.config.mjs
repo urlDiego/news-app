063d932cf380c3ffa5ef8365049726e488a9e11d
urlDiego <diegoarmandogomezvasquez2003@gmail.com>
2026-09-14 16:33:41 -0600

    chore(config): install @astrojs/node adapter and enable SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,16 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
+  output: 'server',
+  vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
