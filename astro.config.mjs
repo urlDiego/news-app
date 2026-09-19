891599650564bbc1c067cae62487c3fe550efcf4
urlDiego <diegoarmandogomezvasquez2003@gmail.com>
2026-09-19 14:42:44 -0600

    chore(config): switch to Vercel adapter for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,7 +11,5 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
 });
