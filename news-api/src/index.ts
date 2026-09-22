e88f436a0b8f2f463a39be75c59fa4312ee60c2a
urlDiego <diegoarmandogomezvasquez2003@gmail.com>
2026-09-22 13:29:27 -0600

    feat(api): enable cors in fastify and consume articles endpoint in astro

--- news-api/src/index.ts
+++ news-api/src/index.ts
@@ -1,14 +1,23 @@
-import Fastify, { type FastifyError } from 'fastify';
+import Fastify, { type FastifyError } from 'fastify'
 import { categoryRoutes } from './routes/categories.js'
 import { articleRoutes } from './routes/articles.js'
+import cors from '@fastify/cors'
 
 const fastify = Fastify({
   logger: true
+})
+
+fastify.register(cors, {
+  origin: 'http://localhost:4321',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
 });
 
 fastify.get('/', async (request, reply) => {
   return { status: 'ok' };
 });
+
+
 
 const start = async () => {
   try {
